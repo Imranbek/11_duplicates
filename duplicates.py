65cb845485aff9d659553d166c44af468327bd4b
Imranbek <imranbek.aliev@pimpay.ru>
2020-02-17 15:43:42 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -2,47 +2,69 @@ import os
 import sys
 
 from collections import defaultdict
+from pathlib import Path
 
 
 def main():
-    folder = None
+    directory = get_directory_from_arguments()
+    file_name_size_paths = collect_files_information_from_directory(directory=directory)
+
+    duplicates = [{name_size: paths} for name_size, paths in file_name_size_paths.items()
+                  if len(paths) > 1]
+    if not len(duplicates):
+        print('Congrats! There is no duplicated files in this directory.')
+        return None
+
+    print('There is some duplicated files: ')
+    print_name_size_paths(name_size_paths=duplicates)
+
+
+def get_directory_from_arguments():
     if len(sys.argv) == 2:
-        folder = sys.argv[1]
-
+        path = sys.argv[1]
     else:
-        exit('Try again with right format "$ python duplicates.py {folder}"')
+        exit('Try again with right format "$ python duplicates.py {directory}"')
 
-    print('Collecting files from the folder, and counting files sizes...')
+    assert check_path_is_a_directory(path=path), 'Path is not a directory, ' \
+                                                 'please, try again with directory path'
 
+    return path
+
+
+def print_name_size_paths(name_size_paths: list):
+    for file_information in name_size_paths:
+        ((file_name, file_size), paths), = file_information.items()
+        print('File "{}" with size {} bytes.'.format(
+            file_name,
+            file_size)
+        )
+        print('Paths:')
+        print(*paths, sep="\n")
+        print('--------------')
+
+
+def collect_files_information_from_directory(directory: str):
+    print('Collecting files from the directory, and counting files sizes...')
     file_name_size_paths = defaultdict(list)
-    for route, directories, file_names in os.walk(folder):
+    for route, directories, file_names in os.walk(directory):
         for file_name in file_names:
-            file_path = '{}/{}'.format(route, file_name)
+            file_path = os.path.join(route, file_name)
             file_size = get_size_of_file(file_path)
 
             file_name_size_paths[(file_name, file_size)].append(file_path)
 
-    duplicates = [name_size for name_size, paths in file_name_size_paths.items()
-                  if len(paths) > 1]
-    if not len(duplicates):
-        print('Congrats! There is no duplicated files in this folder.')
-        return None
-
-    print('There is some duplicated files: ')
-    for file_name_size in duplicates:
-        print('File "{}" with size {} bytes: {} files'.format(
-            file_name_size[0],
-            file_name_size[1],
-            len(file_name_size_paths[file_name_size])
-        )
-        )
-        print('--------------')
+    return file_name_size_paths
 
 
 def get_size_of_file(file_path: str):
     if os.path.isfile(file_path):
-        file_size = os.path.getsize(os.path.abspath(os.path.join(file_path)))
+        file_size = os.path.getsize(file_path)
         return file_size
+
+
+def check_path_is_a_directory(path: str):
+    path_to_check = Path(path)
+    return path_to_check.is_dir()
 
 
 if __name__ == '__main__':
